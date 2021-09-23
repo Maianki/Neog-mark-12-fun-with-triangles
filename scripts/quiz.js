263d35c5782c9418bdb02b5e7dcd46f802d93dd4
Ankit Kumain <kumankit10@gmail.com>
2021-09-23 11:25:40 +0530

    feat: added triangle quiz that shows score at the end

---
+++ scripts/quiz.js
@@ -0,0 +1,20 @@
+
+const btnSubmit = document.querySelector(".btn-submit")
+const quizForm = document.querySelector(".quiz-form")
+const displayOutput = document.querySelector("#display-output")
+const answers= ['1','Equilateral','25','60','45','right triangle','25','isosceles','scalene','acute']
+
+function calculateScore(){
+    var index = 0
+    var score = 0
+    form = new FormData(quizForm)
+    for(var value of form.values()){
+        if(value === answers[index]){
+            score+=1
+        }
+        index+=1
+    }
+    displayOutput.innerText = "Your Score is : "+score
+}
+
+btnSubmit.addEventListener("click",calculateScore)
