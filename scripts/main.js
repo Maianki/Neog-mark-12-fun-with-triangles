dad0f8a4baa50cc482d982d31981d356ef2b6de6
Ankit Kumain <kumankit10@gmail.com>
2021-09-27 20:26:27 +0530

    chore: added validation for angles

--- scripts/main.js
+++ scripts/main.js
@@ -2,13 +2,26 @@ const inputAngles = document.querySelectorAll(".input-angle")
 const btnIsTriangle = document.querySelector(".btn-is-triangle")
 const displayOutput = document.querySelector("#display-output")
 
-
+function validateAngles(){
+    var isInvalidAngle = false
+    for(var i=0;i<3;i++){
+        if(Number(inputAngles[i].value) <= 0 || Number(inputAngles[i].value >=180)){
+           isInvalidAngle = true
+        }
+    }
+    return isInvalidAngle
+}
 function isTriangle(){
-    sumOfAngles = calculateAnglesSum()
-    if(sumOfAngles === 180){
-        displayOutput.innerText = "Hurray! the angles you entered forms a triangle."
+
+    if(validateAngles()){
+        displayOutput.innerText = "An angle cannot be less than equal to zero or greater than equal to 180."
     }else{
-        displayOutput.innerText = "Alas! the angles don't form a triangle"
+        sumOfAngles = calculateAnglesSum()
+        if(sumOfAngles === 180){
+            displayOutput.innerText = "Hurray! the angles you entered forms a triangle."
+        }else{
+            displayOutput.innerText = "Alas! the angles don't form a triangle"
+        }
     }
 }
 
